2417637a25d7fa0e9bb96524e9a6254a670b6ecf
TOmas <eaathand@students.eaaa.dk>
2018-10-10 10:06:24 +0200

    Opret-panes added

---
+++ src/gui/OpretProduktkategoriPane.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+
+public class OpretProduktkategoriPane extends GridPane {
+
+    public OpretProduktkategoriPane() {
+        this.setPadding(new Insets(20));
+        this.setHgap(20);
+        this.setVgap(10);
+        this.setGridLinesVisible(false);
+
+    }
+}
