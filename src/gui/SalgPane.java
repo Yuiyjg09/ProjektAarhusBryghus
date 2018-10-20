1a0928d2574154c0515ddd633ed39e86ec8a199e
TOmas <eaathand@students.eaaa.dk>
2018-10-20 14:15:04 +0200

    SalgsPane added

--- src/gui/SalgPane.java
+++ src/gui/SalgPane.java
@@ -1,6 +1,25 @@
 package gui;
 
+import application.model.Salg;
+import javafx.geometry.Insets;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 
 public class SalgPane extends GridPane {
+    private ListView<Salg> salgListView;
+    private final FileChooser fileChooserSave = new FileChooser();
+
+    public SalgPane() {
+        this.setPadding(new Insets(20));
+        this.setHgap(20);
+        this.setVgap(10);
+        this.setGridLinesVisible(false);
+
+
+    }
+
+    private void updateControls() {
+
+    }
 }
